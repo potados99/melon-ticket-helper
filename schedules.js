10fbb5f7122f1517e989d96119e1ca9619b8879e
potados99 <potados99@gmail.com>
2022-08-13 00:19:38 +0900

    feat: netfunnel과의 맞짱...

--- schedules.js
+++ schedules.js
@@ -3,19 +3,69 @@
  */
 
 async function doScheduleMagic() {
-  const config = {
-    날짜: '18일'
-  };
+  /** 원본 함수를 조금 바꿉니다. */
+  window.reservationZAM = function(prodId, scheduleNo, nfActionId, sessionKey, shuffleKey) {
+    console.log('줄서기 시작한다구~!');
 
-  const sleep = (t) => new Promise(resolve => setTimeout(resolve, t));
+    var oneStopWindowSize = getOneStopSize();
+    NetFunnel_Action({action_id:nfActionId}, {
+      success:function(ev,ret) {
+        window.open("", "onstopForm", oneStopWindowSize);
+        var frm = document.oneForm;
+        var url = 'https://ticket.melon.com/reservation/popup/onestop.htm';
+        frm.action = url;
+        frm.target = 'onstopForm';
+        frm.prodId.value = prodId;
+        frm.pocCode.value = 'SC0002';
+        frm.scheduleNo.value = scheduleNo;
+        frm.sellTypeCode.value = 'ST0001';
+        frm.tYn.value = 'Y';
+        frm.chk.value = encodeURIComponent(sessionKey);
+        frm.shuffleKey.value = encodeURIComponent(shuffleKey);
 
-  /** 날짜를 선택합니다. */
-  $(`#list_date li.item_date:contains("${config.날짜}")`).click()
+        console.log(`key를 빼돌렸다제wwwww [${ret.data.key}]`);
+        const theSecretCode = `(function openNow() {
+          var oneStopWindowSize = getOneStopSize();
+          window.open("", "onstopForm", oneStopWindowSize);
+          var frm = document.oneForm;
+          var url = 'https://ticket.melon.com/reservation/popup/onestop.htm';
+          frm.action = url;
+          frm.target = 'onstopForm';
+          frm.prodId.value = '207126';
+          frm.pocCode.value = 'SC0002';
+          frm.scheduleNo.value = '100002';
+          frm.sellTypeCode.value = 'ST0001';
+          frm.tYn.value = 'Y';
+          frm.chk.value = encodeURIComponent('ED83ACt1ED9484e6mEBB8941p0EBA19DB2l8ockK9DB2l8ey');
+          frm.shuffleKey.value = encodeURIComponent('44446415606466425564');
+          frm.netfunnel_key.value = ":key=" + '${ret.data.key}' + "&";
+          frm.submit();
+        }){}`;
+        console.log(`이렇게 쓰시라구요:\n${theSecretCode}`);
+        alert(`이렇게 쓰시라구요:\n${theSecretCode}`);
 
-  await sleep(200);
+        frm.netfunnel_key.value = ":key=" + ret.data.key + "&";
+        frm.submit();
+      },
+      block: function(ev,ret) {
+        alert(nf_block_msg);
+        return false;
+      },
+      error: function(ev,ret) {
+        alert("다시 시도 부탁 드립니다.");
+        return false;
+      }
+    });
+  }
 
-  /** 예매 창으로 넘어갑니다. */
-  $('#ticketReservation_Btn').click();
+  /** 18일 예약 버튼을 누릅니다. */
+  reservation(
+    '207126', /*prodId*/
+    '100002', /*scheduleNo*/
+    'mypayment', /*nfActionId*/
+    'ED83ACt1ED9484e6mEBB8941p0EBA19DB2l8ockK9DB2l8ey', /*sessionKey*/
+    '44446415606466425564' /*shuffleKey*/
+  );
 }
 
 window.addEventListener('load', function () {
--- seats.js
+++ seats.js
@@ -2,25 +2,92 @@
  * 좌석 선택 화면에서 실행될 스크립트입니다.
  */
 
-function doSeatMagic() {
-  const f = document.getElementById('oneStopFrame').contentWindow;
+/** 제어 플래그 */
+keep = false;
 
-  const config = {
-    좌석등급: '현장수령', /*현장수령S석(2회차), 노란색*/
-    좌석구역: 'Floor 층 17 구역', /*98석 잔여*/
-  };
+/** 적대적 함수 오버라이드 */
+parent.rsrvPopupClose = function () { console.log('팝업을 닫어?? 눈도 깜짝 안한다!'); }
 
-  /** 좌석 등급을 선택합니다. */
-  f.$(`#divGradeSummary tr:contains("${config.좌석등급}")`).click();
+/** 로딩 포착 오버라이드 */
+finishLoading = () => {};
+loadingPromise = Promise.resolve();
 
-  /** 좌석 구역을 선택합니다. */
-  f.$(`#divGradeSummary li:contains("${config.좌석구역}")`).click();
+async function doMagic(f/**frame*/) {
+  /** 새로고침 전에 코드를 조금 바꿔줍니다. */
+  f.setLoading = function (isloading) {
+    if (isloading) {
+      loadingPromise = new Promise((res, rej) => {
+        finishLoading = res;
+        // do nothing...
+      });
+
+      loadingOpen(" ");
+    }else {
+      finishLoading();
+
+      setTimeout(function(){
+        loadingClose();
+      }, 500);
+    }
+  }
+
+  /** 새로고침 합니다. */
+  f.$('#btnReloadSchedule').click();
+  await loadingPromise;
+
+  /** 좌석 구역을 다 펼칩니다. */
+  f.$(`#divGradeSummary tr:contains("VIP")`).first().click();
+  f.$(`#divGradeSummary tr:contains("R")`).first().click();
+  f.$(`#divGradeSummary tr:contains("S")`).first().click();
+  f.$(`#divGradeSummary tr:contains("현장수령")`).first().click();
+  f.$(`#divGradeSummary tr:contains("A")`).first().click();
+
+  /** 좌석 구역 중 자리가 있는 것을 가져옵니다.. */
+  const availableSections = f.$(`#divGradeSummary li span.seat_residual`).filter(function () { return Number.parseInt($(this).text()) !== 0 });
+  if (availableSections.length === 0) {
+    /** 없으면 다음 번에 시도합니다. */
+    console.log('빙빙 돌아가는~ 회전목마처럼~');
+    return true; /*keep going*/
+  }
+
+  /** 첫 번째 좌석 구역을 선택합니다. */
+  availableSections.first().click();
 
   /** 첫 번째 예매 가능한 좌석을 찍습니다. */
-  f.$('#ez_canvas svg rect[fill!="#DDDDDD"]').get(42/*0은 전체 사각형 그 자체*/).dispatchEvent(new Event('click'));
+  f.$('#ez_canvas svg rect[fill!="#DDDDDD"]').get(1/*0은 전체 사각형 그 자체*/).dispatchEvent(new Event('click'));
 
   /** 가격 선택 화면으로 넘어갑니다. */
   f.$('#nextTicketSelection').click();
+
+  /** 작업을 멈춥니다. */
+  console.log('끝나!');
+  return false;
+}
+
+async function runOrStop() {
+  keep = !keep;
+  console.log(keep ? '시작해!' : '멈춰!');
+
+  const f = document.getElementById('oneStopFrame').contentWindow;
+  const sleep = (t) => new Promise(resolve => setTimeout(resolve, t));
+
+  f.$('#viewAll').click();
+
+  await sleep(500);
+
+  while (keep) {
+    try {
+      const needMore = await doMagic(f);
+      if (!needMore) {
+        console.log('>_< 예ㅔ에ㅔㅔ!!!');
+        break;
+      }
+    } catch (e) {
+      console.error('아아악!');
+    }
+
+    await sleep(500);
+  }
 }
 
 window.addEventListener('load', function () {
@@ -28,13 +95,13 @@ window.addEventListener('load', function () {
 
   document.getElementById('oneStopFrame').contentWindow.addEventListener('keypress', function (e) {
     if (e.key === '/') {
-      doSeatMagic();
+      runOrStop();
     }
   }, false);
 
   window.addEventListener('keypress', function (e) {
     if (e.key === '/') {
-      doSeatMagic();
+      runOrStop();
     }
   }, false);
 
