a6622fb1c8898d38890d11517b4bfab0620c3ff1
potados99 <potados99@gmail.com>
2022-08-15 18:49:54 +0900

    chore: 인터벌 50밀리초로 단축,,,

--- seats.js
+++ seats.js
@@ -38,15 +38,15 @@ async function doMagic(f/**frame*/) {
 
   /** 좌석 구역을 다 펼칩니다. */
   f.$(`#divGradeSummary tr:contains("VIP")`).first().click();
-  await sleep(100);
+  await sleep(50);
   f.$(`#divGradeSummary tr:contains("R")`).first().click();
-  await sleep(100);
+  await sleep(50);
   f.$(`#divGradeSummary tr:contains("S")`).first().click();
-  await sleep(100);
+  await sleep(50);
   f.$(`#divGradeSummary tr:contains("현장수령")`).first().click();
-  await sleep(100);
+  await sleep(50);
   f.$(`#divGradeSummary tr:contains("A")`).first().click();
-  await sleep(100);
+  await sleep(50);
 
   /** 좌석 구역 중 자리가 있는 것을 가져옵니다.. */
   const availableSections = f.$(`#divGradeSummary li span.seat_residual`).filter(function () { return Number.parseInt($(this).text()) !== 0 });
@@ -58,11 +58,11 @@ async function doMagic(f/**frame*/) {
 
   /** 첫 번째 좌석 구역을 선택합니다. */
   availableSections.first().click();
-  await sleep(100);
+  await sleep(50);
 
   /** 첫 번째 예매 가능한 좌석을 찍습니다. */
   f.$('#ez_canvas svg rect[fill!="#DDDDDD"]').get(1/*0은 전체 사각형 그 자체*/).dispatchEvent(new Event('click'));
-  await sleep(100);
+  await sleep(50);
 
   /** 가격 선택 화면으로 넘어갑니다. */
   f.$('#nextTicketSelection').click();
